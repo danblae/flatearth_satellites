352d82cccf6c504af6c91c8465e257e3780201fd
Paramatus <32226927+Paramatus@users.noreply.github.com>
2020-10-08 17:38:43 +0200

    Finished code

--- pokusaj55.py
+++ pokusaj55.py
@@ -1,65 +1,38 @@
-f=open("C:\\Users\\7Administrator\\Desktop\\rad\\podacinotpad.txt", "r")
-#print(f.read())
+satellite14 = open("sateliti14.txt", "r")  # Opens the file
+fileLength = len(open("sateliti14.txt", "r").readlines())  # Gets length of the file (Amount of lines in the file)
+count = 0  # Just a counter can be removed
+previousLine = satellite14.readline()  # Reads in the first line of the document
 
 
-sateliti14=open("C:\\Users\\7Administrator\\Desktop\\rad\\sateliti14.txt", "w")
-counter14=0
-
-for line in f:
-    a=line.split()
-    if len(a)==11:
-        if float(a[2])>=-14 and float(a[2])<=14:
-            counter14 += 1
-            sateliti14.write(line)
-            #print line
+# Checks if the lines are created by the same satellite.
+def checkSameSatellite(previous, current):
+    if float(previous.split()[0]) == float(current.split()[0]):
+        return True
+    return False
 
 
-#print counter14
-sateliti14.close()
-
-sateliti14=open("C:\\Users\\7Administrator\\Desktop\\rad\\sateliti14.txt", "r")
-
-
-list=[]
-#for line in sateliti14:
-#    list.append(sateliti14.readline())
-list = sateliti14.readlines()
-i = 1
-count = 0
-print(list.count)
-for x in list:
-    if float(list[i-1].split()[3]) < 180 and float(list[i].split()[3]) > 180:
-        if 180-float(list[i-1].split()[3]) < float(list[i].split()[3]) - 180:
-            print(list[i-1])
-            count += 1
+# Compares the target values in the 4th position from the two lines. and prints the line which is closer to 180 or 0
+def compareValues(previous, current):
+    if float(previous.split()[3]) < 180 < float(current.split()[3]):
+        if 180 - float(previous.split()[3]) < float(current.split()[3]) - 180:
+            print(previousLine)
         else:
-            print(list[i])
-            count += 1
-    elif float(list[i-1].split()[3]) > 300 and float(list[i].split()[3]) < 300:
-        if 360-float(list[i-1].split()[3]) < float(list[i].split()[3]):
-             print(list[i-1])
-             count += 1
+            print(currentLine)
+        return True
+    elif float(previousLine.split()[3]) > 300 > float(currentLine.split()[3]):
+        if 360 - float(previous.split()[3]) < float(current.split()[3]):
+            print(previousLine)
         else:
-            print(list[1])
+            print(currentLine)
+        return True
+    return False
+
+
+# Reads in line after line and compares them with another to print out the needed lines with the value, that is needed
+for i in range(fileLength - 1):
+    currentLine = satellite14.readline()
+    if checkSameSatellite(previousLine, currentLine):
+        if compareValues(previousLine, currentLine):
             count += 1
-    i+=1
-    
-        
-
-# f.seek(0)
-
-# counter2=0
-# sateliti2=open("C:\\Users\\7Administrator\\Desktop\\rad\\sateliti2.txt", "w")
-
-# for line in f:
-#     b=line.split()
-#     if len(b)==11:
-#         if float(b[2])>=-2 and float(b[2])<=2:
-#             counter2 += 1
-#             sateliti2.write(line)
-#             ##print line
-
-# print counter2
-# sateliti2.close()
-
-
+    previousLine = currentLine
+print(count)
